b241b84047fb0006274c54ab509ba16e9fd11257
Adrián <adrianmonzonf@gmail.com>
2021-03-15 13:06:46 +0100

    nodemailer commit

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ module.exports = (app) => {
     app.use("/api/services", require("./service.routes.js"));
     app.use("/api/users", require("./user.routes.js"));
     app.use("/api/auth", require("./auth.routes.js"));
-    // app.use("/api/mail", require("./mail.routes.js"));
+    app.use("/api/mail", require("./mail.routes.js"));
     app.use("/api/files", require("./files.routes.js"));
 };
